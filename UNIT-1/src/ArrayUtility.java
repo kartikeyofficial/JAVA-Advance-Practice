fec94ef417a02ae19abbe33e7913e4443495c1b9
kartikey <kartikeygithubproject@gmail.com>
2025-11-13 00:52:21 +0530

    JAVA Advance Practice

--- UNIT-1/src/ArrayUtility.java
+++ UNIT-1/src/ArrayUtility.java
@@ -5,8 +5,19 @@ public class ArrayUtility {
         Scanner x = new Scanner(System.in);
         System.out.println("Please Enter The Number Of Element:");
         int size = x.nextInt();
+        int[] numArray = new int[size];
         for(int i =0; i<size;i++){
             System.out.print("Please Enter The No. "+(i+1)+": ");
+            numArray[i] = x.nextInt();
+
     }
+        return numArray;
     }
+    public static int[] displayArray(int[] numArray){
+        for(int i= 0; i<numArray.length;i++){
+            System.out.print(numArray[i]+", ");
+        }
+        return numArray;
+    }
+
 }
