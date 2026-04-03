04f59ebf5634af800dc994a99d4bf7d299a8407a
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-04-03 23:12:52 +0530

    MultiThreading

--- Multi Threading And Executor Service/src/in/KartikeyCoding/MultiThreading/ThreadClass/NeedOfMultiThreading.java
+++ Multi Threading And Executor Service/src/in/KartikeyCoding/MultiThreading/ThreadClass/NeedOfMultiThreading.java
@@ -23,6 +23,13 @@ public class NeedOfMultiThreading {
         }
         System.out.println("\nFourth Task is Completed!");
 
+        for (int i=1;i<=1000;i++){
+            System.out.print(i+"& ");
+        }
+        System.out.println("\nFifth Task is Completed!");
+
+
+
         long endTime = System.currentTimeMillis();
         System.out.println("Total Time Taken: "+((endTime-startTime)));
 
