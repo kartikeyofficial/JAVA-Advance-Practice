7718512c23705d973bb83bc4c546755215577e19
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-04-06 10:57:49 +0530

    user Input writer File Handling

--- Programming Challenges/src/in/KartikeyCoding/Challenge49/TestWritter.java
+++ Programming Challenges/src/in/KartikeyCoding/Challenge49/TestWritter.java
@@ -2,13 +2,18 @@ package in.KartikeyCoding.Challenge49;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class TestWritter {
     static void main(String[] args) {
         String fileName = "Kartikey.txt";
+        Scanner x = new Scanner(System.in);
+        System.out.println("Enter the String in the File: ");
+        String writ = x.nextLine();
+
 
         try(FileWriter writer = new FileWriter(fileName)){
-            writer.write("Kartikey is a GoodBoy.");
+            writer.write(writ + System.lineSeparator());
             writer.flush();
             System.out.println("File is Successfully Write");
         }
