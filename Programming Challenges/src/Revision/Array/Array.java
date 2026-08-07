e434eb3f525a94e6e929068dda60e956893a9634
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-08-08 00:48:56 +0530

    Arrays

---
+++ Programming Challenges/src/Revision/Array/Array.java
@@ -0,0 +1,17 @@
+package Revision.Array;
+
+public class Array {
+    static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 10;
+        arr[1] = 11;
+        arr[2] = 12;
+        arr[3] = 13;
+        arr[4] = 14;
+
+        for (int i=0;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
---
+++ Programming Challenges/src/Revision/Array/ArraySearching.java
@@ -0,0 +1,22 @@
+package Revision.Array;
+
+import java.util.Scanner;
+
+public class ArraySearching {
+    static void main(String[] args) {
+        System.out.println("welcome to Array Searching");
+        int[] nums={1,2,3,4,5,6,7,8,9,10};
+        Scanner x = new Scanner(System.in);
+        System.out.print("Enter the Number Do You Want to Search: ");
+        int key = x.nextInt();
+        for (int i=0;i<nums.length;i++){
+            if(nums[i]==key){
+                System.out.println("That the Number "+key+" is Present at Index: "+i);
+               
+            }
+            else {
+                System.out.println("That the Number "+key+" is not Present in Array");
+            }
+        }
+    }
+}
