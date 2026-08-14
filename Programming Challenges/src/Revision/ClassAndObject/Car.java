65bd7131737dc37c44f4092cfa4656c8174c79e5
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-08-14 23:54:06 +0530

    Class and Object

---
+++ Programming Challenges/src/Revision/ClassAndObject/Car.java
@@ -0,0 +1,15 @@
+package Revision.ClassAndObject;
+
+public class Car {
+    int wheels;
+    int doors;
+    int windows;
+    String color;
+
+    public Car(int wheels,int doors,int windows,String color){
+        this.wheels = wheels;
+        this.doors = doors;
+        this.windows = windows;
+        this.color = color;
+    }
+}
