6c30404a1ef0fb4e9e0057a59e69802a165da532
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-09-03 23:23:36 +0530

    DoWhile

--- Programming Challenges/src/Revision/Control_Statement/DoWhile.java
+++ Programming Challenges/src/Revision/Control_Statement/DoWhile.java
@@ -2,6 +2,10 @@ package Revision.Control_Statement;
 
 public class DoWhile {
     static void main(String[] args) {
-        
+        int i=0;
+        do{
+            System.out.print(i+" ");
+            i++;
+        }while (i<10);
     }
 }
