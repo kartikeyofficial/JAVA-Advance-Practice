1a6bee598b25a78557cd71382abdb527094cd7aa
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-09-07 23:40:27 +0530

    To String Method

---
+++ Programming Challenges/src/Revision/ToString/Car.java
@@ -0,0 +1,24 @@
+package Revision.ToString;
+
+public class Car {
+    int noOfWheels;
+    int noOfDoors;
+    int maxSpeed;
+    String name;
+    String modelNumber;
+    String company;
+
+    public Car(int noOfWheels, int noOfDoors, int maxSpeed, String name, String modelNumber, String company) {
+        this.noOfWheels = noOfWheels;
+        this.noOfDoors = noOfDoors;
+        this.maxSpeed = maxSpeed;
+        this.name = name;
+        this.modelNumber = modelNumber;
+        this.company = company;
+    }
+
+    static void main(String[] args) {
+        Car swift = new Car(4,4,200,"Suzuki Swift","0987654321","Maruti");
+        System.out.println(swift.toString());
+    }
+}
